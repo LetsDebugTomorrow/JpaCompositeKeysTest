2111574f83d5eb44167d9c35b07864a86bfb9d8e
TangHaomiao <1020145095@qq.com>
2021-09-15 21:22:01 +0800

    initialize project

---
+++ src/main/java/com/example/jpacompositekeystest/JpaCompositeKeysTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpacompositekeystest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCompositeKeysTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCompositeKeysTestApplication.class, args);
+    }
+
+}
